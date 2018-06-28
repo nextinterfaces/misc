1d2435079bb85c74d460ac661405b15a39c2377e
Atanas Roussev <nextinterfaces@users.noreply.github.com>
2018-06-28 09:50:16 -0700

    Create await.js

---
+++ await.js
@@ -0,0 +1,9 @@
+async function logic (x) {
+  try {
+       const y = await (y) => (y + 1)
+       const y2 = await (y2) => (y2 + 1)
+       ...
+  } catch(e) {
+     console.log('Error handling ..', e)
+  }
+}
