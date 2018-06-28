aef7e3aae084966aea479d263f9ac33dd1552bef
Atanas Roussev <nextinterfaces@users.noreply.github.com>
2018-06-28 09:59:16 -0700

    Update promise1.js

--- promise1.js
+++ promise1.js
@@ -4,13 +4,16 @@ new Promise((a) => {
   throw new Error('ee')
 })
 .then(console.log)
-.catch(console.log)
+.catch(console.log) // the fix
 
-I would rather use:
+// I would rather use:
 
 async () => {
    try {
-      throw new Error('ee')
+      await () => function1_block
+      await () => function2_block
+      await () => function3_block
+      await () => throw new Error('ee')
    } catch(e){
       console.log(e)
    }
